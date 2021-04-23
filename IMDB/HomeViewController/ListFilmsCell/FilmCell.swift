fe899e9a2b27747fb2e140f80ef0bfeeb2bf04a7
nxh98 <nxh98@gmail.com>
2021-04-23 14:30:47 +0700

    Create project

---
+++ IMDB/HomeViewController/ListFilmsCell/FilmCell.swift
@@ -0,0 +1,23 @@
+//
+//  FilmCell.swift
+//  IMDB
+//
+//  Created by NXH on 4/22/21.
+//
+
+import UIKit
+
+class FilmCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
